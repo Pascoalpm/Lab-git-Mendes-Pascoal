f1fb282353e10dbdb3312adb2173c971772e92e7
Pascoal Mendes <pascoalp.mendes@gmail.com>
2025-09-16 23:11:00 +0300

    Add condition for empty name

--- programa.py
+++ programa.py
@@ -2,6 +2,10 @@
 
 def print_greeting():
 name = input("What is your name?")
+
+if name == "":
+print("Hello, Anonymous User!")
+else:
 for i in range(3):
 message = f"Hello, {name}! Greeting #{i+1}"
 print(message)
